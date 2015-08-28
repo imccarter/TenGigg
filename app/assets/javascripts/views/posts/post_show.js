b2d444d3ecc62d6ce98162635bbb942901f4b423
Ivan McCarter <ivanmccarter@gmail.com>
2015-08-27 22:09:08 -0700

    Another error display

--- app/assets/javascripts/views/posts/post_show.js
+++ app/assets/javascripts/views/posts/post_show.js
@@ -75,6 +75,11 @@ TenGigg.Views.PostShow = Backbone.CompositeView.extend({
   },
 
   toggleVote: function (e) {
+  	this.$('.vote-error').empty();
+  	if (!TenGigg.CURRENT_USER.id) {
+  		this.$('.vote-error').append("You need to log in to vote!");
+  		return;
+  	}
     var score;
     if ($(e.currentTarget).attr('name') === 'upvote') {
       score = 1;
